077a8945bb2566f74030ada32668e5a467f9f9de
птичка <Sapunova270301@gmail.com>
2022-12-12 08:24:56 +0300

    add MILI.iml

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 3500;
+
+        int miles = amount / 20;
+        System.out.println("Бонусные мили: " + miles);
+
+    }
+}
